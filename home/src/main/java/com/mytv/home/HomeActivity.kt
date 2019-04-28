527c3d9bb3b7283aef90783c44cc9811c04d2d23
Gabriel Rodriguez <gabriel.rodriguez3103@gmail.com>
2019-04-27 22:33:19 -0500

    :star: Improve navigation implementation

--- home/src/main/java/com/mytv/home/HomeActivity.kt
+++ home/src/main/java/com/mytv/home/HomeActivity.kt
@@ -2,13 +2,13 @@ package com.mytv.home
 
 import android.os.Bundle
 import android.view.Menu
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.navigation.NavController
-import androidx.navigation.Navigation
-import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.mytv.series.base.ui.navigation.setupWithNavController
 import dagger.android.AndroidInjection
 import dagger.android.AndroidInjector
 import dagger.android.DispatchingAndroidInjector
@@ -20,17 +20,25 @@ class HomeActivity : AppCompatActivity(), HasSupportFragmentInjector {
     @Inject
     lateinit var fragmentInjector: DispatchingAndroidInjector<Fragment>
 
-    private lateinit var navController : NavController
+    private lateinit var navController: NavController
+
+    private val isNavControllerInitialized: Boolean get() = ::navController.isInitialized
 
     override fun onCreate(savedInstanceState: Bundle?) {
         AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
-        setSupportActionBar(findViewById(R.id.tbHome))
-        navController = Navigation.findNavController(this, R.id.navController)
-        NavigationUI.setupWithNavController(findViewById<BottomNavigationView>(R.id.bnHome), navController)
-        NavigationUI.setupActionBarWithNavController(this, navController)
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bnHome)
+        bottomNavigationView.setupWithNavController(
+            listOf(R.navigation.home),
+            supportFragmentManager,
+            R.id.flNavController,
+            intent
+        ).observe(this, Observer { navController ->
+            setupActionBarWithNavController(navController)
+            this.navController = navController
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -39,14 +47,18 @@ class HomeActivity : AppCompatActivity(), HasSupportFragmentInjector {
         return true
     }
 
-    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
-        return item?.let {
-            NavigationUI.onNavDestinationSelected(it, navController) || super.onOptionsItemSelected(item)
-        } ?: super.onOptionsItemSelected(item)
+    override fun onBackPressed() {
+        if (isNavControllerInitialized && !navController.popBackStack()) {
+            super.onBackPressed()
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        return NavigationUI.navigateUp(navController, null)
+        return if (isNavControllerInitialized) {
+            navController.navigateUp()
+        } else {
+            false
+        }
     }
 
     override fun supportFragmentInjector(): AndroidInjector<Fragment> = fragmentInjector
